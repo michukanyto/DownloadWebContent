b19af979651ea14c1ba4fee81b45f6a5ebaac714
Marlon <escobar.marlon@gmail.com>
2019-04-24 17:28:24 -0400

    First commit

---
+++ app/src/main/java/com/appsmontreal/downloadingwebcontent/MainActivity.java
@@ -0,0 +1,77 @@
+package com.appsmontreal.downloadingwebcontent;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import model.DownloadTask;
+
+public class MainActivity extends AppCompatActivity {
+    
+    TextView resultTextView;
+    TextView urlTextView;
+    Button downloadButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+        try {
+            result = task.execute("https://www.w3schools.com").get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Log.i("Result",result);
+        initialize();
+
+    }
+
+    private void initialize() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/model/DownloadTask.java
@@ -0,0 +1,63 @@
+package model;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.appsmontreal.downloadingwebcontent.R;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DownloadTask extends AsyncTask<String, Void, String> {
+
+    protected String result;
+
+
+
+    public String getResult() {
+        return result;
+    }
+//
+//    public void setResult(String result) {
+//       this.result = result;
+//    }
+//
+//
+//    public DownloadTask(String url) {
+//       setResult(url);
+//    }
+
+    @Override
+    protected String doInBackground(String... urls) {
+
+        String result = "";
+        URL url;
+        HttpURLConnection urlConnection = null;
+
+        try {
+            url = new URL(urls[0]);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = urlConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(in);
+            int data = reader.read();
+
+            while (data != -1) {
+                char current = (char) data;
+
+                result += current;
+
+                data = reader.read();
+            }
+
+            return result;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return "Failed";
+        }
+    }
+}
